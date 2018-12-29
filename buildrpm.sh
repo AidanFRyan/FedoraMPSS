c54feb291d910261f0f1ed201bcc1c06a11a4a04
Guitarshredder1 <aidan.ryan4@gmail.com>
2018-12-29 15:03:05 -0500

    small

--- buildrpm.sh
+++ buildrpm.sh
@@ -3,3 +3,4 @@ cp mpss-modules-3.8.4.tar.bz2 ~/rpmbuild/SOURCES/.
 rpmbuild -bb ~/rpmbuild/SPECS/mpss-modules.spec
 cp ~/rpmbuild/RPMS/x86_64/* ~/Downloads/mpss-3.8.4/modules
 cp ~/Downloads/mpss-3.8.4/modules/* ~/Downloads/mpss-3.8.4/.
+rm mpss-modules-3.8.4.tar.bz2
